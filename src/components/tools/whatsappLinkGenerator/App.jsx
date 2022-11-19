d120c431b9b988bf91dcb709108641c6b0c9c81f
Luciano Canziani <96893895+LucianoCanziani@users.noreply.github.com>
2022-11-19 14:51:45 -0300

    wsp 4

--- src/components/tools/whatsappLinkGenerator/App.jsx
+++ src/components/tools/whatsappLinkGenerator/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
-import { WhatsappLinkGenerator } from './components/WhatsappLinkGenerator';
-import { Info } from './components/Info';
+import  WhatsappLinkGenerator  from './components/WhatsappLinkGenerator';
+import Info  from './components/Info';
 import { ToastContainer, toast } from 'react-toastify';
 const App = () => {
 
